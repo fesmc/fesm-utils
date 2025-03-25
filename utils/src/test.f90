135787acd4b10c8779eb7bc8ff60a4368b9c65af
Alexander Robinson <alexander.robinson@awi.de>
2025-03-25 14:31:47 +0100

    GQ: removed cism routines, now relying on gq object methods (gq2D, gq3D). The get_basis_function_derivatives routines still come directly from cism and use the global variables available, but these should also eventually be modified to use the objects and remove the global variables entirely.

--- utils/src/test.f90
+++ utils/src/test.f90
@@ -13,56 +13,39 @@ program test
     integer  :: i, j 
     integer  :: im1, ip1, jm1, jp1
 
-if (.TRUE.) then
-    call gq2D_init(gq2D)
-
-    var(1,1) = 2.0
-    var(1,2) = 2.0
-    var(2,1) = 4.0
-    var(2,2) = 4.0
-
-    dx = 1.0
-    dy = 1.0 
+    logical, parameter :: test_2D = .TRUE.
+    logical, parameter :: test_3D = .FALSE.
     
-    i = 2
-    j = 2
-    im1 = i-1
-    ip1 = i
-    jm1 = j-1
-    jp1 = j
+    if (test_2D) then
+        call gq2D_init(gq2D,verbose=.TRUE.)
 
-    call gq2D_to_nodes(gq2D, var_qp, var, dx, dy, "ab",i,j,im1,ip1,jm1,jp1)
+        var(1,1) = 2.0
+        var(1,2) = 2.0
+        var(2,1) = 4.0
+        var(2,2) = 4.0
 
-    write(*,*) "var_qp: ", var_qp
+        dx = 1.0
+        dy = 1.0 
+        
+        i = 2
+        j = 2
+        im1 = i-1
+        ip1 = i
+        jm1 = j-1
+        jp1 = j
+
+        call gq2D_to_nodes(gq2D, var_qp, var, dx, dy, "ab",i,j,im1,ip1,jm1,jp1)
+
+        write(*,*) "var_qp: ", var_qp
     
+    end if
+
     write(*,*)
     write(*,*) "----"
     write(*,*)
 
-    !call gq3D_init(gq3D)
-end if
-
-    ! ==== CISM style ====
-if (.FALSE.) then
-    write(*,*)
-    write(*,*) "----------------------------------------"
-    write(*,*)
-    write(*,*) "CISM style..."
-    write(*,*)
-
-    call gaussian_quadrature_init()
-
-    var(1,1) = 2.0
-    var(1,2) = 2.0
-    var(2,1) = 4.0
-    var(2,2) = 4.0
-
-    dx = 1.0
-    dy = 1.0 
-
-    call gaussian_quadrature_2D_to_nodes(var_qp,var,dx,dy,i=2,j=2,grid_type="ab")
-
-    write(*,*) "var_qp: ", var_qp
-end if
+    if (test_3D) then
+        call gq3D_init(gq3D,verbose=.TRUE.)
+    end if
 
 end program test
